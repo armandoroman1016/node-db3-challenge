8ea11c4316b13c476afd625496bff0c9beeec5a4
Armando Roman <armandoroman1016@gmail.com>
2019-09-11 14:01:20 -0700

    update model functioning

---
+++ schemes/scheme-model.js
@@ -0,0 +1,44 @@
+const db = require('../data/db-config.js')
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update
+}
+
+function find(){
+    return db('schemes')
+    .then( schemes => schemes)
+}
+
+function findById(id){
+    return db('schemes')
+    .where({id: id})
+    .first()
+    .then( scheme => scheme)
+}
+
+function findSteps(id){
+    return db('schemes')
+    .join('steps', 'steps.scheme_id', 'schemes.id')
+    .where({'scheme_id': id})
+    .orderBy('steps.step_number')
+    .then( steps => steps)
+}
+
+function add(newScheme){
+    return db('schemes')
+    .insert(newScheme)
+    .then( ([id]) => findById(id) )
+}
+
+function update(updatedScheme, id){
+    return db('schemes')
+    .where({id: id})
+    .update({
+        scheme_name: updatedScheme.scheme_name
+    })
+    .then(updated => findById(id))
+}
